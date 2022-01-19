8897dd9673cb7a50cc98fe8460b16e19d61a2f07
Sarafina-Rafidi <sarafina_rafidi@outlook.com>
2022-01-19 16:22:31 +0800

    Section 2 - Add Data Annotations

--- InAndOut/Models/Item.cs
+++ InAndOut/Models/Item.cs
@@ -1,8 +1,13 @@
-﻿namespace InAndOut.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InAndOut.Models
 {
     public class Item
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
         public string ItemName { get; set; }
         public string Borrower { get; set; }
         public string Lender { get; set; }
