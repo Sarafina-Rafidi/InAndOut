cfd2683797b00afd2b6a13a2f5e7748e08502a1f
Sarafina-Rafidi <sarafina_rafidi@outlook.com>
2022-01-19 16:20:08 +0800

    Section 2 - Create Item Model

---
+++ InAndOut/Models/Item.cs
@@ -0,0 +1,10 @@
+﻿namespace InAndOut.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string ItemName { get; set; }
+        public string Borrower { get; set; }
+        public string Lender { get; set; }
+    }
+}
