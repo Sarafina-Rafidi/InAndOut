0e8e6fdeb91f293afa02040c95128053b9f371b5
Sarafina-Rafidi <sarafina_rafidi@outlook.com>
2022-01-21 10:25:27 +0800

    Section 3 - Add Item Controller Edit

--- InAndOut/Controllers/ItemController.cs
+++ InAndOut/Controllers/ItemController.cs
@@ -21,15 +21,57 @@ namespace InAndOut.Controllers
             return View(objItemList);
         }
 
+
+
+        //Create - GET
         public IActionResult Create()
         {
             return View();
         }
 
+        //Create - POST
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Item obj)
         {
+            if (ModelState.IsValid)
+            {
+                _db.Items.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Item created successfully";
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+
+
+        //Edit - GET
+        public IActionResult Edit(int? id)
+        {
+            if (id==null || id==0)
+            {
+                return NotFound();
+            }
+
+            var itemFromDb = _db.Items.Find(id);
+
+
+            if (itemFromDb==null)
+            {
+                return NotFound();
+            }
+
+            return View(itemFromDb);
+        }
+
+        //Edit - POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Item obj)
+        {
+            
+
             if (ModelState.IsValid)
             {
                 _db.Items.Add(obj);
